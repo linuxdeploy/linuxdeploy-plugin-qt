b04df41150ee076e274935a757dfe2bd03279a50
TheAssassin <theassassin@assassinate-you.net>
2018-07-07 05:12:21 +0200

    Build i386 AppImages

--- travis/build-appimage.sh
+++ travis/build-appimage.sh
@@ -3,6 +3,11 @@
 set -e
 set -x
 
+if [ "$ARCH" == "" ]; then
+    echo "Error: $ARCH is not set"
+    exit 1
+fi
+
 # use RAM disk if possible
 if [ "$CI" == "" ] && [ -d /dev/shm ]; then
     TEMP_BASE=/dev/shm
@@ -26,19 +31,23 @@ OLD_CWD=$(readlink -f .)
 
 pushd "$BUILD_DIR"
 
-cmake "$REPO_ROOT" -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=RelWithDebInfo
+if [ "$ARCH" == "i386" ]; then
+    EXTRA_CMAKE_ARGS=("-DCMAKE_TOOLCHAIN_FILE=$REPO_ROOT/cmake/toolchains/i386-linux-gnu.cmake")
+fi
+
+cmake "$REPO_ROOT" -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=RelWithDebInfo "${EXTRA_CMAKE_ARGS[@]}"
 
 make -j$(nproc)
 
 make install DESTDIR=AppDir
 
-wget https://github.com/TheAssassin/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+wget https://github.com/TheAssassin/linuxdeploy/releases/download/continuous/linuxdeploy-"$ARCH".AppImage
 chmod +x linuxdeploy*.AppImage
-./linuxdeploy-x86_64.AppImage -n linuxdeploy-plugin-qt --appdir AppDir --init-appdir \
+./linuxdeploy-"$ARCH".AppImage -n linuxdeploy-plugin-qt --appdir AppDir --init-appdir \
     -d "$REPO_ROOT"/resources/linuxdeploy-plugin-qt.desktop \
     -i "$REPO_ROOT"/resources/linuxdeploy-plugin-qt.svg \
     -e $(which patchelf) \
     -e $(which strip) \
     --output appimage
 
-mv linuxdeploy-plugin-qt*.AppImage "$OLD_CWD"/
+mv linuxdeploy-plugin-qt-"$ARCH".AppImage "$OLD_CWD"/
