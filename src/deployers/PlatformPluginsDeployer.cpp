84797c0e05e6904f6ceb90312be277b360074b08
Nikos Chantziaras <realnc@gmail.com>
2020-02-15 13:02:57 +0200

    Deploy gtk3 and xdgdesktopportal themes if found

--- src/deployers/PlatformPluginsDeployer.cpp
+++ src/deployers/PlatformPluginsDeployer.cpp
@@ -57,25 +57,26 @@ bool PlatformPluginsDeployer::deploy() {
         // either loading succeeds, then the system Gtk shall be used anyway, otherwise loading fails and Qt will fall
         // back to the default UI theme
         // we don't care whether this works (it's an experimental feature), therefore we ignore the return values
-        const auto libqgtk2Filename = "libqgtk2.so";
-        const auto libqgtk2styleFilename = "libqgtk2style.so";
+        const auto libqgtk2Path = platformThemesPath / "libqgtk2.so";
+        const auto libqgtk3Path = platformThemesPath / "libqgtk3.so";
+        const auto libqxdgPath = platformThemesPath / "libqxdgdesktopportal.so";
 
-        const auto libqgtk2Path = platformThemesPath / libqgtk2Filename;
-        const auto libqgtk2stylePath = stylesPath / libqgtk2styleFilename;
-
-        // we need to check whether the files exist at least, otherwise the deferred deployment operation fails
-        if (bf::is_regular_file(libqgtk2Path)) {
-            ldLog() << "Attempting to deploy" << libqgtk2Filename << "found at path" << libqgtk2Path << std::endl;
-            appDir.deployFile(libqgtk2Path, platformThemesDestination);
-        } else {
-            ldLog() << "Could not find" << libqgtk2Filename << "on system, skipping deployment" << std::endl;
+        for (const auto &file : {libqgtk2Path, libqgtk3Path, libqxdgPath}) {
+            // we need to check whether the files exist at least, otherwise the deferred deployment operation fails
+            if (bf::is_regular_file(file)) {
+                ldLog() << "Attempting to deploy" << file.filename() << "found at path" << file.parent_path() << std::endl;
+                appDir.deployFile(file, platformThemesDestination);
+            } else {
+                ldLog() << "Could not find" << file.filename() << "on system, skipping deployment" << std::endl;
+            }
         }
 
+        const auto libqgtk2stylePath = stylesPath / "libqgtk2style.so";
         if (bf::is_regular_file(libqgtk2stylePath)) {
-            ldLog() << "Attempting to deploy" << libqgtk2styleFilename << "found at path" << libqgtk2stylePath << std::endl;
+            ldLog() << "Attempting to deploy" << libqgtk2stylePath.filename() << "found at path" << libqgtk2stylePath << std::endl;
             appDir.deployFile(libqgtk2stylePath, stylesDestination);
         } else {
-            ldLog() << "Could not find" << libqgtk2styleFilename << "on system, skipping deployment" << std::endl;
+            ldLog() << "Could not find" << libqgtk2stylePath.filename() << "on system, skipping deployment" << std::endl;
         }
     }
 
