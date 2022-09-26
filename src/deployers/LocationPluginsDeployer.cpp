c6587540b4754ea1262a41dfacd312a9f7587007
Tadej Novak <tadej@tano.si>
2022-09-26 19:47:36 +0200

    Deploy QtLocation geoservices plugins

---
+++ src/deployers/LocationPluginsDeployer.cpp
@@ -0,0 +1,28 @@
+// system headers
+#include <filesystem>
+
+// library headers
+#include <linuxdeploy/core/log.h>
+
+// local headers
+#include "LocationPluginsDeployer.h"
+
+using namespace linuxdeploy::plugin::qt;
+using namespace linuxdeploy::core::log;
+
+namespace fs = std::filesystem;
+
+bool LocationPluginsDeployer::deploy() {
+    // calling the default code is optional, but it won't hurt for now
+    if (!BasicPluginsDeployer::deploy())
+        return false;
+
+    ldLog() << "Deploying Location plugins" << std::endl;
+
+    for (fs::directory_iterator i(qtPluginsPath / "geoservices"); i != fs::directory_iterator(); ++i) {
+        if (!appDir.deployLibrary(*i, appDir.path() / "usr/plugins/geoservices/"))
+            return false;
+    }
+
+    return true;
+}
