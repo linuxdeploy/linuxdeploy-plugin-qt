756fa6b0f90e7cad3b4a24f543bc2ded4804de21
Sushi Fish <sushi@sushitee.de>
2024-02-14 10:15:59 +0100

    added waylandcompositor plugin deployer

---
+++ src/deployers/WaylandcompositorPluginsDeployer.cpp
@@ -0,0 +1,38 @@
+// system headers
+#include <filesystem>
+
+// library headers
+#include <linuxdeploy/core/log.h>
+
+// local headers
+#include "WaylandcompositorPluginsDeployer.h"
+
+using namespace linuxdeploy::plugin::qt;
+using namespace linuxdeploy::core::log;
+
+namespace fs = std::filesystem;
+
+bool WaylandcompositorPluginsDeployer::deploy() {
+    // calling the default code is optional, but it won't hurt for now
+    if (!BasicPluginsDeployer::deploy())
+        return false;
+
+    ldLog() << "Deploying waylandcompositor plugin" << std::endl;
+
+    for (fs::directory_iterator i(qtPluginsPath / "wayland-decoration-client"); i != fs::directory_iterator(); ++i) {
+        if (!appDir.deployLibrary(*i, appDir.path() / "usr/plugins/wayland-decoration-client/"))
+            return false;
+    }
+
+    for (fs::directory_iterator i(qtPluginsPath / "wayland-graphics-integration-client"); i != fs::directory_iterator(); ++i) {
+        if (!appDir.deployLibrary(*i, appDir.path() / "usr/plugins/wayland-graphics-integration-client/"))
+            return false;
+    }
+
+    for (fs::directory_iterator i(qtPluginsPath / "wayland-shell-integration"); i != fs::directory_iterator(); ++i) {
+        if (!appDir.deployLibrary(*i, appDir.path() / "usr/plugins/wayland-shell-integration/"))
+            return false;
+    }
+
+    return true;
+}
